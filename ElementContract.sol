0e9231917b2a8ef9d747c3b60b71c41d4112b652
Gabriel Alejandro Maldonado Hernandez <gabriel.amhx@gmail.com>
2021-03-04 19:33:34 -0600

    Funcion abonar v2.0

--- ElementContract.sol
+++ ElementContract.sol
@@ -3,10 +3,10 @@ pragma solidity ^0.4.24;
 contract ElementContract {
     address private owner = msg.sender;
     
-    //Function to deposit in the contract 
+    //Function to deposit in the contract. 80% owner, 20% contract
     function depositContract(uint amount)public payable{
         require(msg.value == amount);
-        
+        owner.transfer((amount*80)/100);
     }
     
    //Function to get the balance of the contract
@@ -22,5 +22,11 @@ contract ElementContract {
             "Sender not authorized."
         );
         _;
+    }
+    
+    //Function to tranfer an amount of cash to an owner account
+    function transfer(uint amount) public onlyBy(owner){
+            require(address(this).balance >= amount);
+            owner.transfer(amount);
     }
 }
