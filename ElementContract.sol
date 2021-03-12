16eea01e450961d66429511c1d9f4d2c8da33a80
Gabo1988 <gabriel.amhx@gmail.com>
2021-03-11 19:33:38 -0600

    Update ElementContract.sol

--- ElementContract.sol
+++ ElementContract.sol
@@ -2,11 +2,35 @@ pragma solidity ^0.4.24;
 
 contract ElementContract {
     address private owner = msg.sender;
+    struct Account{
+        string name;
+        string lastName;
+        address addr;
+        uint numberTrx;
+        uint depositAmount;
+        uint depositCounter;
+    }
+    
+    mapping(address => Account) private accounts;
+    mapping(address => bool) private joinedAccounts;
     
     //Function to deposit in the contract. 80% owner, 20% contract
-    function depositContract(uint amount)public payable{
+    function depositContract(string name, string lastName, uint amount)public payable{
         require(msg.value == amount);
         owner.transfer((amount*80)/100);
+        join(name, lastName, amount);
+    }
+    
+    //Function to save data of personal accounts in map accounts
+    function join(string name, string lastName, uint amount) public{
+        Account storage account = accounts[msg.sender];
+        account.name = name;
+        account.lastName = lastName;
+        account.addr = msg.sender;
+        account.numberTrx = 1;
+        account.depositAmount = amount;
+        account.depositCounter = 0;
+        joinedAccounts[msg.sender] = true;
     }
     
    //Function to get the balance of the contract
@@ -16,17 +40,28 @@ contract ElementContract {
     
     //Function to tranfer an amount of cash to an owner account
     function transferOwner(uint amount) public onlyBy(owner){
-            require(address(this).balance >= amount,
-             "Fund insufficient.");
+            require(address(this).balance >= amount);
             owner.transfer(amount);
     }
     
     //Function to tranfer an amount of cash to an account
     function transferTo(uint amount, address to)public onlyBy(owner){
-        require(address(this).balance >= amount,
-        "Fund insufficient.");
+        require(address(this).balance >= amount);
         require(to != address(0));
         to.transfer(amount);
+    }
+    
+    //Function to get info about personal accounts
+    function getAccount(address addr) public view returns(string, string, address, uint, uint, uint){
+        require(accountJoined(msg.sender),
+            "Account not registered.");
+        Account memory account = accounts[addr];
+        return (account.name, account.lastName, account.addr, account.numberTrx, account.depositAmount, account.depositCounter);
+    }
+    
+    //Function to validate if an account exist
+    function accountJoined(address addr) private view returns(bool){
+        return joinedAccounts[addr];
     }
     
     //Function for owner
