7085e04265db2812e0ef6bebd003be1b9d27ae1f
JoelDG92 <delgadojoel8@gmail.com>
2021-03-25 20:08:55 -0600

    [add] Dispersión V 0.4
    #Valida que no disperse beneficios mas de 12 veces a una inversión 
    #Calcular el monto a dispersar de acuerdo a un factor 
    #Realiza la dispersión a todas las inversiones
    #Validar suficiencia de fondos para la dispersión total
    #Modifica el % de beneficio a dispersar

--- ElementContract.sol
+++ ElementContract.sol
@@ -2,107 +2,140 @@ pragma solidity ^0.8.3;
 
 contract ElementContract {
     address private owner = msg.sender;
-    uint profitPercent = 0;
-    
-    struct Account{
+    uint256 profitPercent = 0;
+
+    struct Account {
         address addr;
-        uint numberTrx;
-        uint depositAmount;
-        uint depositCounter;
-        uint depositYields;
+        uint256 numberTrx;
+        uint256 depositAmount;
+        uint256 depositCounter;
+        uint256 depositYields;
     }
-    
+
     mapping(address => Account[]) private accounts;
     mapping(address => bool) private joinedAccounts;
-    
+    address[] internal keyList;
+
     //Function to deposit in the contract: 80% owner, 20% contract
-    function invest(uint amount)public payable{
+    function invest(uint256 amount) public payable {
         require(msg.value == amount);
-        payable(owner).transfer((amount*80)/100);
+        payable(owner).transfer((amount * 80) / 100);
         join(amount);
     }
-    
-   //Function to save data of personal accounts in map accounts
-    function join(uint amount) private{
-        if(accountJoined(msg.sender)){
-            uint count = accounts[msg.sender].length;
-            Account memory account = accounts[msg.sender][count-1];
-            uint numberTrxAux = (account.numberTrx+1);
-            
-            accounts[msg.sender].push(Account(msg.sender,numberTrxAux,amount,0,0));
-        }else{
-            accounts[msg.sender].push(Account(msg.sender,1,amount,0,0));
+
+    //Function to save data of personal accounts in map accounts
+    function join(uint256 amount) private {
+        if (accountJoined(msg.sender)) {
+            uint256 count = accounts[msg.sender].length;
+            Account memory account = accounts[msg.sender][count - 1];
+            uint256 numberTrxAux = (account.numberTrx + 1);
+
+            accounts[msg.sender].push(
+                Account(msg.sender, numberTrxAux, amount, 0, 0)
+            );
+        } else {
+            accounts[msg.sender].push(Account(msg.sender, 1, amount, 0, 0));
+            keyList.push(msg.sender);
         }
         joinedAccounts[msg.sender] = true;
     }
-    
+
     //Function to deposit yields in an Account
-    function depositYields( uint amount,address to,uint transactionNumber) private{
-         Account storage accountAux = accounts[to][transactionNumber-1];
-         uint numberTrx = accountAux.numberTrx;
-         uint depositAmount =accountAux.depositAmount;
-         uint depositCounter = (accountAux.depositCounter+1);
-         uint depostiYields =(accountAux.depositYields+amount);
-         accounts[to][transactionNumber-1]=(Account(to,numberTrx,depositAmount,depositCounter,depostiYields));
+    function depositYields(
+        uint256 amount,
+        address to,
+        uint256 transactionNumber
+    ) private {
+        Account storage accountAux = accounts[to][transactionNumber - 1];
+        uint256 numberTrx = accountAux.numberTrx;
+        uint256 depositAmount = accountAux.depositAmount;
+        uint256 depositCounter = (accountAux.depositCounter + 1);
+        uint256 depostiYields = (accountAux.depositYields + amount);
+        accounts[to][transactionNumber - 1] = (
+            Account(to, numberTrx, depositAmount, depositCounter, depostiYields)
+        );
     }
-    
-    
-   //Function to get the balance of the contract
-   function getBalance() public view onlyBy(owner) returns(uint)  {
+
+    //Function to get the balance of the contract
+    function getBalance() public view onlyBy(owner) returns (uint256) {
         return (address(this).balance);
     }
-    
+
     //Function to tranfer an amount of cash to an owner account
-    function transferToOwner(uint amount) public onlyBy(owner){
-        require(address(this).balance >= amount,
-            "Insufficient balance amount");
+    function transferToOwner(uint256 amount) public onlyBy(owner) {
+        require(address(this).balance >= amount, "Insufficient balance amount");
         payable(owner).transfer(amount);
     }
-    
+
     //Function to tranfer an amount of cash to an account
-    function transferTo(address to, uint transactionNumber)public onlyBy(owner){
-        require(accountJoined(to),
-            "Account not registered.");
-        require(to != address(0));
-        Account memory accountAux = accounts[to][transactionNumber-1];
-        if(accountAux.depositCounter < 12){
-            uint amount = (accountAux.depositAmount*profitPercent)/100;
-            depositYields(amount,to,transactionNumber);
-            payable(to).transfer(amount);
-        }else{
-            revert("This transaction exceeds the number of deposits limit");
+    function transferTo() public onlyBy(owner) {
+        //require(accountJoined(to), "Account not registered.");
+        //require(to != address(0));
+
+        uint256 sum = 0;
+        uint256 amount = 0;
+        for (uint256 i = 0; i < keyList.length; i++) {
+            address addrAux = keyList[i];
+            for (uint256 j = 0; j < accounts[addrAux].length; j++) {
+                Account memory accountAux = accounts[addrAux][j];
+                if (accountAux.depositCounter < 12) {
+                    amount = (accountAux.depositAmount * profitPercent) / 100;
+                    sum = sum + amount;
+                }
+            }
+        }
+        amount = 0;
+        if (sum > 0) {
+            if (getBalance() >= sum) {
+                for (uint256 i = 0; i < keyList.length; i++) {
+                    address addrAux = keyList[i];
+                    for (uint256 j = 0; j < accounts[addrAux].length; j++) {
+                        Account memory accountAux = accounts[addrAux][j];
+                        if (accountAux.depositCounter < 12) {
+                            amount =
+                                (accountAux.depositAmount * profitPercent) /
+                                100;
+                            depositYields(
+                                amount,
+                                addrAux,
+                                accountAux.numberTrx
+                            );
+                            payable(addrAux).transfer(amount);
+                        }
+                    }
+                }
+            } else {
+                revert("This transaction exceeds the number of deposits limit");
+            }
+        } else {
+            revert("No benefits to deposit");
         }
     }
-    
+
     //Function to get info about personal accounts
-    function getAccountBalance() public view returns(Account[] memory){
-        require(accountJoined(msg.sender),
-            "Account not registered.");
-        uint count = accounts[msg.sender].length;  
+    function getAccountBalance() public view returns (Account[] memory) {
+        require(accountJoined(msg.sender), "Account not registered.");
+        uint256 count = accounts[msg.sender].length;
         Account[] memory array = new Account[](count);
-        for (uint i = 0; i < count; i++) {
+        for (uint256 i = 0; i < count; i++) {
             array[i] = accounts[msg.sender][i];
         }
         return array;
     }
-    
+
     //Function to validate if an account exist
-    function accountJoined(address addr) private view returns(bool){
+    function accountJoined(address addr) private view returns (bool) {
         return joinedAccounts[addr];
     }
-    
+
     //Function to modify profitPercent
-    function modifyProfitPercent(uint percent) public{
+    function modifyProfitPercent(uint256 percent) public {
         profitPercent = percent;
     }
-    
+
     //Function for owner
-    modifier onlyBy(address _account)
-    {
-        require(
-            msg.sender == _account,
-            "Sender not authorized."
-        );
+    modifier onlyBy(address _account) {
+        require(msg.sender == _account, "Sender not authorized.");
         _;
     }
 }
